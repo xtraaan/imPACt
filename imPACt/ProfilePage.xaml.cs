e45ee1cca27648a8275c9e4e03551f536b73e647
Nadin <37120180+nadk18@users.noreply.github.com>
2019-11-25 18:20:42 -0600

    Update ProfilePage.xaml.cs

--- imPACt/ProfilePage.xaml.cs
+++ imPACt/ProfilePage.xaml.cs
@@ -14,8 +14,7 @@ namespace imPACt
     {
         public ProfilePage()
         {
-            InitializeComponent();
-            string name = $"{App.currentUser.FirstName} {App.currentUser.LastName}";
+            BindingContext = new ProfilePageViewModel();
         }
     }
 }
