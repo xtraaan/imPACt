05f776ee2acf43faf12637bca5c5987937bc8974
rachpi <46795679+rachpi@users.noreply.github.com>
2019-11-27 22:31:45 -0600

    Create News.xaml.cs

---
+++ imPACt/News.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace imPACt
+{
+    public partial class News : ContentPage
+    {
+        public News()
+        {
+            InitializeComponent();
+        }
+    }
+}
