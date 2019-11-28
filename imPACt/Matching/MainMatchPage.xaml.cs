fed060edc6e9856fa183e5f9d40637855c438b63
Eilynn Tran <eilynntran@gmail.com>
2019-11-27 19:18:54 -0600

    Added CarouselPage for Matching

---
+++ imPACt/Matching/MainMatchPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace imPACt.Matching
+{
+    public partial class MainMatchPage : CarouselPage
+    {
+        public MainMatchPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- imPACt/Matching/MatchPage.xaml.cs
+++ imPACt/Matching/MatchPage.xaml.cs
@@ -49,21 +49,8 @@ namespace imPACt.Matching
 
             if (list.Count() != 0)
                 listView.ItemsSource = list;
-            //else
-            //    await DisplayAlert("No Matches Found", "Try again later...", "Okay", "Cancel");
-
-
-            List<RegisterUserTable> matches = new List<RegisterUserTable>();
-
-            foreach (var user in App.currentUser.Matches.ToList())
-            {
-                if (user.Major == App.currentUser.Major)
-                {
-                    matches.Add(user);
-                }
-            }
-
-            listView2.ItemsSource = matches;
+            else
+                await DisplayAlert("No Matches Found", "Try again later...", "Okay", "Cancel");
         }
 
 
