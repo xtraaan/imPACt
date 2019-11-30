ce0ade75db28c5ffd5415b28a26a84594d19256d
rachpi <46795679+rachpi@users.noreply.github.com>
2019-11-29 20:11:22 -0600

    Update EventsFormPage.xaml.cs

--- imPACt/EventsFormPage.xaml.cs
+++ imPACt/EventsFormPage.xaml.cs
@@ -11,5 +11,10 @@ namespace imPACt
         {
             InitializeComponent();
         }
+
+        async void OnCreateEventButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EventsFormPage());
+        }
     }
 }
