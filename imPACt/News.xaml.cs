7509467f072a0ada3f3a1d24d4587c0147cd5246
rachpi <46795679+rachpi@users.noreply.github.com>
2019-11-29 21:57:59 -0600

    Update News.xaml.cs

--- imPACt/News.xaml.cs
+++ imPACt/News.xaml.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace imPACt
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class News : ContentPage
     {
         public News()
         {
             InitializeComponent();
+
+            this.BindingContext = this;
+
+            newsName.Text = App.currentUser.NewsTitle;
+            newsDescription.Text = App.currentUser.NewsDescription;
+            newsDate.Text = App.currentUser.NewsDate;
+            newsAuthor.Text = App.currentUser.FullName;
         }
     }
+
 }
