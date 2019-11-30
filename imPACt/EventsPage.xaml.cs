c10d17ef5e93fab24699270606018ffdc15984e5
rachpi <46795679+rachpi@users.noreply.github.com>
2019-11-29 20:13:11 -0600

    Update EventsPage.xaml.cs

--- imPACt/EventsPage.xaml.cs
+++ imPACt/EventsPage.xaml.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace imPACt
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EventsPage : ContentPage
     {
         public EventsPage()
         {
             InitializeComponent();
+
+            eventName.Text = App.currentUser.EventName;
+            eventDescription.Text = App.currentUser.EventDescription;
+            eventDate.Text = App.currentUser.EventDate;
+            eventSponsor.Text = App.currentUser.EventSponsor;
+            //this.BindingContext = this;
         }
     }
 }
