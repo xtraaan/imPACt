0c35d249b04f4a342be02304b99e7490236fa9d6
rachpi <46795679+rachpi@users.noreply.github.com>
2019-12-01 23:09:50 -0600

    Create AllEventsLayoutPage.xaml.cs

---
+++ imPACt/AllEventsLayoutPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace imPACt
+{
+    public partial class AllEventsLayoutPage : ContentPage
+    {
+        public AllEventsLayoutPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private async void CreateButton(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EventsFormPage());
+        }
+    }
+}
