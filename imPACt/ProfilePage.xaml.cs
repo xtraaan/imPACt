e41bd770ab1fa195287d5ada4e6b4f778bb368b1
Nadin <37120180+nadk18@users.noreply.github.com>
2019-12-02 01:08:27 -0600

    Update ProfilePage.xaml.cs

--- imPACt/ProfilePage.xaml.cs
+++ imPACt/ProfilePage.xaml.cs
@@ -18,11 +18,11 @@ namespace imPACt
             NavigationPage.SetHasNavigationBar(this, false);
 
             //BindingContext = new ProfilePageViewModel();
-            name.Text = App.currentUser.FirstName + " " + App.currentUser.LastName;
-            researchInterests.Text = App.currentUser.ResearchInterest;
-            major.Text = App.currentUser.Major;
-            gradeYear.Text = App.currentUser.Year;
-
+            name.Text = "  " + App.currentUser.FirstName + " " + App.currentUser.LastName;
+            researchInterests.Text = "  " + App.currentUser.ResearchInterest;
+            major.Text = "  " + App.currentUser.Major;
+            gradeYear.Text = "  " + App.currentUser.Year;
+            school.Text = "  " + App.currentUser.School;
 
         }
     }
