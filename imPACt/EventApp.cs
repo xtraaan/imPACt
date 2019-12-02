42a681754311e6a5446a357acd121c0b5bab7d25
rachpi <46795679+rachpi@users.noreply.github.com>
2019-12-02 11:40:35 -0600

    Create EventApp.cs

---
+++ imPACt/EventApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace imPACt
+{
+    public class EventApp
+    {
+        public int ImpactEventId { get; set; }
+
+        public string EventName { get; set; }
+
+        public string Sponsor { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public EventApp(int impactEventId, string eventName, string sponsor, string pictureUrl)
+        {
+            ImpactEventId = impactEventId;
+            EventName = eventName;
+            Sponsor = sponsor;
+            PictureUrl = pictureUrl;
+        }
+
+        public EventApp()
+        {
+        }
+    }
+}
