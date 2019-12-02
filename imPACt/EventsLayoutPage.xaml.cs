0cd1910208f3b32c3d626cad904790a49280bdf9
rachpi <46795679+rachpi@users.noreply.github.com>
2019-12-01 22:20:29 -0600

    Create EventsLayoutPage.xaml.cs

---
+++ imPACt/EventsLayoutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace imPACt
+{
+    public partial class EventsLayoutPage : ContentPage
+    {
+        public EventsLayoutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
