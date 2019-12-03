56abb1314eccb0e7d30223db4be026aa1c8f13e6
rachpi <46795679+rachpi@users.noreply.github.com>
2019-12-03 00:23:02 -0600

    Create NewsFormPage.xaml.cs

---
+++ imPACt/NewsFormPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using imPACt.Tables;
+using System;
+using System.Collections.Generic;
+using SQLiteNetExtensions.Attributes;
+using SQLite;
+using SQLiteNetExtensions;
+using SQLiteNetExtensions.Extensions;
+using Xamarin.Forms;
+
+namespace imPACt
+{
+    public partial class NewsFormPage : ContentPage
+    {
+        public NewsFormPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void CreateButton(object sender, EventArgs e)
+        {
+            var News1 = new News()
+            {
+                NewsTitle = newsName.Text,
+                NewsDescription = newsDescription.Text,
+                NewsAuthor = newsAuthor.Text
+            };
+
+            await App.Database.SaveNewsAsync(News1);
+
+            await Navigation.PopAsync();
+        }
+
+        private async void CancelButton(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
